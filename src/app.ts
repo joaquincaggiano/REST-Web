6a6ddf6d84a3148877e47be148cd9cc6988b2d5c
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-04-20 16:24:12 +0200

    express

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,8 @@
-import http2 from "http2";
-import fs from "fs";
+import { Server } from "./presentation/server";
 
-const server = http2.createSecureServer(
-  {
-    key: fs.readFileSync("./keys/server.key"),
-    cert: fs.readFileSync("./keys/server.crt"),
-  },
-  (req, res) => {
-    if (req.url === "/") {
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.write(fs.readFileSync("./public/index.html", "utf-8"));
-      res.end();
-      return;
-    }
+const main = async () => {
+  const server = new Server();
+  await server.start();
+};
 
-    if (req.url?.endsWith(".js")) {
-      res.writeHead(200, { "Content-Type": "text/javascript" });
-    } else if (req.url?.endsWith(".css")) {
-      res.writeHead(200, { "Content-Type": "text/css" });
-    }
-
-    try {
-      const responseContent = fs.readFileSync(`./public/${req.url}`, "utf-8");
-      res.end(responseContent);
-
-    } catch (error) {
-      res.writeHead(404, { "Content-Type": "text/html" });
-      res.end();
-    }
-  }
-);
-
-server.listen(8080, () => {
-  console.log("Server is running on port 8080");
-});
+main();
---
+++ src/presentation/server.ts
@@ -0,0 +1,13 @@
+import express from "express";
+
+export class Server {
+  private app = express();
+
+  async start() {
+    this.app.use(express.static("public"));
+
+    this.app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  }
+}
