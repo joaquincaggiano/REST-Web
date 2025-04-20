8828fae422fc383c324dd15b8fb527cbba413215
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-04-20 15:25:49 +0200

    responder con archivos css y js

--- src/app.ts
+++ src/app.ts
@@ -15,10 +15,17 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/html" });
     res.write(fs.readFileSync("./public/index.html", "utf-8"));
     res.end();
-  } else {
-    res.writeHead(404, { "Content-Type": "text/html" });
-    res.end();
+    return;
   }
+
+  if (req.url?.endsWith(".js")) {
+    res.writeHead(200, { "Content-Type": "text/javascript" });
+  } else if (req.url?.endsWith(".css")) {
+    res.writeHead(200, { "Content-Type": "text/css" });
+  }
+
+  const responseContent = fs.readFileSync(`./public/${req.url}`, "utf-8");
+  res.end(responseContent);
 });
 
 server.listen(8080, () => {
