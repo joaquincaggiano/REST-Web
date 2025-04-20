fc772b886522ab9cf83e5d5b7775574a16dc8aab
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-04-20 16:13:07 +0200

    creando certificados ssl

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,35 @@
-import http from "http";
+import http2 from "http2";
 import fs from "fs";
-const server = http.createServer((req, res) => {
-  console.log(req.url);
-  //   res.writeHead(200, { "Content-Type": "text/html" });
-  //   res.write(`<h1>URL: ${req.url}</h1>`);
-  //   res.end();
 
-  //   const data = { name: "John Doe", age: 30, city: "New York" };
-  //   res.writeHead(200, { "Content-Type": "application/json" });
-  //   res.write(JSON.stringify(data));
-  //   res.end();
+const server = http2.createSecureServer(
+  {
+    key: fs.readFileSync("./keys/server.key"),
+    cert: fs.readFileSync("./keys/server.crt"),
+  },
+  (req, res) => {
+    if (req.url === "/") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(fs.readFileSync("./public/index.html", "utf-8"));
+      res.end();
+      return;
+    }
 
-  if (req.url === "/") {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(fs.readFileSync("./public/index.html", "utf-8"));
-    res.end();
-    return;
+    if (req.url?.endsWith(".js")) {
+      res.writeHead(200, { "Content-Type": "text/javascript" });
+    } else if (req.url?.endsWith(".css")) {
+      res.writeHead(200, { "Content-Type": "text/css" });
+    }
+
+    try {
+      const responseContent = fs.readFileSync(`./public/${req.url}`, "utf-8");
+      res.end(responseContent);
+
+    } catch (error) {
+      res.writeHead(404, { "Content-Type": "text/html" });
+      res.end();
+    }
   }
-
-  if (req.url?.endsWith(".js")) {
-    res.writeHead(200, { "Content-Type": "text/javascript" });
-  } else if (req.url?.endsWith(".css")) {
-    res.writeHead(200, { "Content-Type": "text/css" });
-  }
-
-  const responseContent = fs.readFileSync(`./public/${req.url}`, "utf-8");
-  res.end(responseContent);
-});
+);
 
 server.listen(8080, () => {
   console.log("Server is running on port 8080");
