e1e165b87d58432a81db7ac83c6d795fb2082c5c
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-04-20 16:49:30 +0200

    utilizando SPA

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,10 +1,20 @@
 import express from "express";
+import path from "path";
 
 export class Server {
   private app = express();
 
   async start() {
     this.app.use(express.static("public"));
+
+    this.app.get("/", (req, res) => {
+      const indexPath = path.join(__dirname, "../../public/index.html");
+      res.sendFile(indexPath);
+    });
+
+    this.app.use((req, res) => {
+      res.sendFile(path.join(process.cwd(), "public", "index.html"));
+    });
 
     this.app.listen(3000, () => {
       console.log("Server is running on port 3000");
