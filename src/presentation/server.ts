a6f15cbc108431b0764ce762ea91be2d3d017312
Joaquin Serra caggiano <joaquinserracaggiano@MacBook-Pro-de-Joaquin-2.local>
2025-04-25 15:11:22 +0200

    configurando prisma en etsting y terminar el endpoint api/todos

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -9,6 +9,7 @@ interface ServerOptions {
 
 export class Server {
   public readonly app = express();
+  private serverListener?: any;
   private readonly port: number;
   private readonly publicPath: string;
   private readonly routes: Router;
@@ -36,8 +37,12 @@ export class Server {
     });
 
     // Start server
-    this.app.listen(this.port, () => {
+    this.serverListener = this.app.listen(this.port, () => {
       console.log(`Server is running on port ${this.port}`);
     });
+  }
+
+  public close() {
+    this.serverListener?.close();
   }
 }
--- tests/presentation.todos/routes.test.ts
+++ tests/presentation.todos/routes.test.ts
@@ -1,14 +1,32 @@
 import request from "supertest";
 import { testServer } from "../test-server";
+import { prisma } from "../../src/data/postgres";
 
 describe("Todos Routes", () => {
   beforeAll(async () => {
     await testServer.start();
   });
 
+  afterAll(() => {
+    testServer.close();
+  });
+
+  const todo1 = { title: "Hola mundo 1" };
+  const todo2 = { title: "Hola mundo 2" };
+
   test("should return a list of todos", async () => {
-    const response = await request(testServer.app)
+    await prisma.todo.deleteMany();
+    await prisma.todo.createMany({
+      data: [todo1, todo2],
+    });
+
+    const { body } = await request(testServer.app)
       .get("/api/todos")
       .expect(200);
+
+    expect(body).toBeInstanceOf(Array);
+    expect(body.length).toBe(2);
+    expect(body[0].title).toBe(todo1.title);
+    expect(body[1].title).toBe(todo2.title);
   });
 });
